106de96e4feafc76e663b533279584f9eaae9f94
happyguest666top <145270176+happyguest666top@users.noreply.github.com>
2025-11-10 19:40:30 +0200

    Add files via upload

--- storeproject/store/views.py
+++ storeproject/store/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 from store.models import Creator
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse('Привіт це мій перший views!')
+    #return HttpResponse('Привіт це мій перший views!')
+    return render(request, 'home.html')
 
 def creators(request):
     creators_obj = Creator.objects.all()
-    return HttpResponse(creators_obj)
+    #return HttpResponse(creators_obj)
+    context = {'creators': creators_obj}
+    return render(request, 'creators.html', context)
 
 def creator(request, pk):
     creators_obj = Creator.objects.filter(id=pk)
-    return HttpResponse(creators_obj)
+    #return HttpResponse(creators_obj)
+    context = {'creator': creators_obj}
+    return render(request, 'creator.html', context)
