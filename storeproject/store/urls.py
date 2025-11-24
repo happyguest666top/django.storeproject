a30274fdb7da9d0c8f411ca2000d46a6f9038230
happyguest666top <145270176+happyguest666top@users.noreply.github.com>
2025-11-24 19:47:20 +0200

    Add files via upload

--- storeproject/store/urls.py
+++ storeproject/store/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from store.views import home, creators, creator
+from .views import home, creators, creator, login_view, logout_view, register_view
 
 urlpatterns = [
     path('home/', home, name='store-home'),
     path('creators/', creators, name='store-creators'),
     path('creator/<int:pk>/', creator, name='store-creator'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
 
 app_name = 'store'
--- storeproject/store/views.py
+++ storeproject/store/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from store.models import Creator
-from django.shortcuts import render
+from .models import Creator
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, LoginForm
 
 
 def home(request):
@@ -14,7 +16,45 @@ def creators(request):
     return render(request, 'creators.html', context)
 
 def creator(request, pk):
-    creators_obj = Creator.objects.filter(id=pk)
+    creators_obj = Creator.objects.filter(id=pk).first()
     #return HttpResponse(creators_obj)
     context = {'creator': creators_obj}
     return render(request, 'creator.html', context)
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('school:login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('store:store-home')  # змінити на вашу сторінку
+            else:
+                return render(request, 'login.html', {
+                    'form': form,
+                    'error': 'Невірний логін або пароль'
+                })
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('store:login')
